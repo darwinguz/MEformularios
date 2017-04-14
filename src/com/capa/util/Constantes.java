7ca2e8811df54f86306409e1c796a5cf688c38d8
FREDDY <FREDDY@FREDDY-PC>
2017-04-14 00:27:52 -0500

    Constante ficha MaTemp y atributo en query detalle

--- src/com/capa/util/Constantes.java
+++ src/com/capa/util/Constantes.java
@@ -46,6 +46,52 @@ public class Constantes {
 	public static final String LBL_HS_39 = "<html><body><p align='right'>Bomba de 3 hp instalada</p></body></html>";
 	public static final String LBL_HS_40 = "<html><body><p align='right'>1 set accesorios</p></body></html>";
 	public static final String LBL_HS_41 = "<html><body><p align='right'>Tanque de 55 gl y Cisterna 30 m3</p></body></html>";
+
+	/*
+	 * Labels MaTemplate
+	 */
+
+	public static final String LBL_MA_0 = "<html><body><p align='right'>Luminarias LED  60w-5000 lum  </p></body></html>";
+	public static final String LBL_MA_1 = "<html><body><p align='right'>Tomacorrientes  </p></body></html>";
+	public static final String LBL_MA_2 = "<html><body><p align='right'>Interruptor  </p></body></html>";
+	public static final String LBL_MA_3 = "<html><body><p align='right'>Kit cable eléctrico  </p></body></html>";
+	public static final String LBL_MA_4 = "<html><body><p align='right'>Detector de Humo  </p></body></html>";
+	public static final String LBL_MA_5 = "<html><body><p align='right'>Canal plástico PVC de L=3m  </p></body></html>";
+	public static final String LBL_MA_6 = "<html><body><p align='right'>Kit de accesorios de conexión Agua de Lluvia (2 rejila, 4 codos 90°, 1/4 gl polipega, tornillos, abrazaderas)</p></body></html>";
+	public static final String LBL_MA_7 = "<html><body><p align='right'>Tubería de PVC 75mm L=3m  </p></body></html>";
+	public static final String LBL_MA_8 = "<html><body><p align='right'>Tubería de PVC 75mm L=0.6m  </p></body></html>";
+	public static final String LBL_MA_9 = "<html><body><p align='right'>Kit para conexión de tubería 75 mm. (lija pliego, polipega 1/4 gal, 1 codo 90°) </p></body></html>";
+	public static final String LBL_MA_10 = "<html><body><p align='right'>Puertas  </p></body></html>";
+	public static final String LBL_MA_11 = "<html><body><p align='right'>Bisagras  </p></body></html>";
+	public static final String LBL_MA_12 = "<html><body><p align='right'>Perfil SF Tipo 12 MA-SF-12 para puerta y Tipo 12 MS-SF 12 paredes</p></body></html>";
+	public static final String LBL_MA_13 = "<html><body><p align='right'>Ventana corredera, compuesta por tres hojas y dos carriles. UPVC con vidrio flotado de 3 mm de 3x1,10 </p></body></html>";
+	public static final String LBL_MA_14 = "<html><body><p align='right'>Perfil SF Tipo 12 MA-SF-12 para ventana  </p></body></html>";
+	public static final String LBL_MA_15 = "<html><body><p align='right'>Dados de cimentación de postes AXAcm</p></body></html>";
+	public static final String LBL_MA_16 = "<html><body><p align='right'>Luminarias LED 120 W - 12000 lum</p></body></html>";
+	public static final String LBL_MA_17 = "<html><body><p align='right'>Postes de 6 mts simples</p></body></html>";
+	public static final String LBL_MA_18 = "<html><body><p align='right'>Kit cableado eléctrico AE</p></body></html>";
+	public static final String LBL_MA_19 = "<html><body><p align='right'>Kit de cableado eléctrico BA</p></body></html>";
+	public static final String LBL_MA_20 = "<html><body><p align='right'>Tubería PVC 2´para cableado l=6m</p></body></html>";
+	public static final String LBL_MA_21 = "<html><body><p align='right'>Cajas de Conexión  0,60x0,60 mt</p></body></html>";
+	public static final String LBL_MA_22 = "<html><body><p align='right'>Paneles vynil piso (0,15x1,93 m)  </p></body></html>";
+	public static final String LBL_MA_23 = "<html><body><p align='right'>Paneles EPS para cubierta I=7, a= 1 m  </p></body></html>";
+	public static final String LBL_MA_24 = "<html><body><p align='right'>Paneles vynil piso (0,15x1,93 m)  </p></body></html>";
+	public static final String LBL_MA_25 = "<html><body><p align='right'>Paneles EPS para cubierta I=7, a= 1 m  </p></body></html>";
+	public static final String LBL_MA_26 = "<html><body><p align='right'>Kit de cableado eléctrico BA</p></body></html>";
+	public static final String LBL_MA_27 = "<html><body><p align='right'>Dados cimentación  </p></body></html>";
+	public static final String LBL_MA_28 = "<html><body><p align='right'>Perfil SF Tipo 1  </p></body></html>";
+	public static final String LBL_MA_29 = "<html><body><p align='right'>Perfil SF Tipo 2  </p></body></html>";
+	public static final String LBL_MA_30 = "<html><body><p align='right'>Perfil SF Tipo 3</p></body></html>";
+	public static final String LBL_MA_31 = "<html><body><p align='right'>Perfil SF Tipo 4  </p></body></html>";
+	public static final String LBL_MA_32 = "<html><body><p align='right'>Perfil SF Tipo 5  </p></body></html>";
+	public static final String LBL_MA_33 = "<html><body><p align='right'>Perfil SF Tipo 6  </p></body></html>";
+	public static final String LBL_MA_34 = "<html><body><p align='right'>Perfil SF Tipo 7  </p></body></html>";
+	public static final String LBL_MA_35 = "<html><body><p align='right'>Perfil SF Tipo 8  </p></body></html>";
+	public static final String LBL_MA_36 = "<html><body><p align='right'>Perfil SF Tipo 9  </p></body></html>";
+	public static final String LBL_MA_37 = "<html><body><p align='right'>Perfil SF Tipo 10  </p></body></html>";
+	public static final String LBL_MA_38 = "<html><body><p align='right'>Perfil SF Tipo 11  </p></body></html>";
+	public static final String LBL_MA_39 = "<html><body><p align='right'>Perfil SF Tipo 12  </p></body></html>";
+
 	/*
 	 * TxtCantidadLimite HSTemplate
 	 */
@@ -91,4 +137,49 @@ public class Constantes {
 	public static final String TXT_HS_39 = "1";
 	public static final String TXT_HS_40 = "1";
 	public static final String TXT_HS_41 = "1";
+
+	/*
+	 * TxtCantidadLimite MaTemplate
+	 */
+	public static final String TXT_MA_0 = "9";
+	public static final String TXT_MA_1 = "2";
+	public static final String TXT_MA_2 = "1";
+	public static final String TXT_MA_3 = "1";
+	public static final String TXT_MA_4 = "1";
+	public static final String TXT_MA_5 = "3";
+	public static final String TXT_MA_6 = "2";
+	public static final String TXT_MA_7 = "2";
+	public static final String TXT_MA_8 = "1";
+	public static final String TXT_MA_9 = "1";
+	public static final String TXT_MA_10 = "1";
+	public static final String TXT_MA_11 = "3";
+	public static final String TXT_MA_12 = "11";
+	public static final String TXT_MA_13 = "3";
+	public static final String TXT_MA_14 = "3";
+	public static final String TXT_MA_15 = "5";
+	public static final String TXT_MA_16 = "1";
+	public static final String TXT_MA_17 = "1";
+	public static final String TXT_MA_18 = "3";
+	public static final String TXT_MA_19 = "3";
+	public static final String TXT_MA_20 = "6";
+	public static final String TXT_MA_21 = "6";
+	public static final String TXT_MA_22 = "6";
+	public static final String TXT_MA_23 = "6";
+	public static final String TXT_MA_24 = "1";
+	public static final String TXT_MA_25 = "177";
+	public static final String TXT_MA_26 = "9";
+	public static final String TXT_MA_27 = "15";
+	public static final String TXT_MA_28 = "2";
+	public static final String TXT_MA_29 = "2";
+	public static final String TXT_MA_30 = "20";
+	public static final String TXT_MA_31 = "6";
+	public static final String TXT_MA_32 = "40";
+	public static final String TXT_MA_33 = "40";
+	public static final String TXT_MA_34 = "2";
+	public static final String TXT_MA_35 = "2";
+	public static final String TXT_MA_36 = "20";
+	public static final String TXT_MA_37 = "6";
+	public static final String TXT_MA_38 = "40";
+	public static final String TXT_MA_39 = "32";
+
 }
