f3f6c35bf9254c87be57c74159a272cdfc568e60
MiguelAngel <MiguelAngel@MiguelWin>
2017-04-25 22:50:42 -0500

    Corrección ingreso datos ficha d a la BD

--- src/com/capa/negocios/ComponenteFichaD.java
+++ src/com/capa/negocios/ComponenteFichaD.java
@@ -1,6 +1,7 @@
 package com.capa.negocios;
 
 import java.sql.ResultSet;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
@@ -8,6 +9,10 @@ import com.capa.datos.TCabecera;
 import com.capa.datos.TFichaD;
 
 public class ComponenteFichaD implements ServicioFichaD {
+
+	String mensaje = null;
+	String titulo = null;
+	int tipoMensaje = 0;
 
 	@Override
 	public void crear(TFichaD fichaD) {
@@ -35,5 +40,24 @@ public class ComponenteFichaD implements ServicioFichaD {
 			System.out.println("Error al BUSCAR: " + e.getMessage());
 		}
 		return maximo;
+	}
+
+	@Override
+	public void crearFichaD(List<TFichaD> listaFichas) {
+		// TODO Auto-generated method stub
+		listaFichas.forEach(item -> {
+			try {
+				String query = "INSERT INTO t_ficha_d (c_serial, f_descripcion, f_foto_ficha_d, fd_actualizacion_n, io_serial) VALUES (?,?,?,?,?);";
+				Query.insertarGenerico(query, new Object[] { item.gettCabecera().getCSerial(), item.getfDescripcion(),
+						item.getfDfotoPath(), item.getfDactualizacionN(), item.gettInformacionObl().getIoSerial() });
+				mensaje = "Ingreso exitoso";
+				titulo = "INGRESO";
+				tipoMensaje = 1;
+				// System.out.println(query);
+			} catch (Exception e) {
+				mensaje = "Error al insertar datos del formulario MA " + e.getMessage();
+			}
+		});
+		JOptionPane.showMessageDialog(null, mensaje, titulo, tipoMensaje);
 	}
 }
